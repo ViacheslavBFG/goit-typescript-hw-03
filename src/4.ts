6fe323ccc41f939af910b4ce0b4e19fe99dc7de5
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2023-10-09 20:26:15 +0300

    1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,88 @@
-const key = new Key();
+// const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+// const house = new MyHouse(key);
+// const person = new Person(key);
 
-house.openDoor(person.getKey());
+// house.openDoor(person.getKey());
 
-house.comeIn(person);
+// house.comeIn(person);
 
 
-export {};
+// export {};
+
+
+
+
+
+interface IPerson {
+    getKey(): Key;
+  }
+  
+  interface IHouse {
+    door: boolean;
+    key: Key;
+    tenants: Person[];
+  
+    comeIn(person: Person): void;
+    openDoor(key: Key): void;
+  }
+  
+  class Key {
+    private signature: number = Math.random();
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  
+  class Person implements IPerson {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      console.log(this.key);
+      return this.key;
+    }
+  }
+  
+  
+  abstract class House implements IHouse {
+    door: boolean = false;
+    key: Key;
+    tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log("Person has come in the House");
+      }
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      const Opened = key.getSignature() === this.key.getSignature();
+      this.door = Opened;
+      console.log(Opened ? "Door is open." : "Door is accessible.");
+    }
+  }
+  
+  
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
+  
+  export {};
